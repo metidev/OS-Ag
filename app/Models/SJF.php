6203007a2e222ec58ad044c7be701f28df2bedeb
metidev <anwrymhdy@gmail.com>
2024-12-16 12:48:13 +0330

    fix SJF algorithm

--- app/Models/SJF.php
+++ app/Models/SJF.php
@@ -10,16 +10,44 @@ class SJF
     }
     public function calculate()
     {
-
         usort($this->processes, function ($a, $b) {
-            return $a['burst_time'] <=> $b['burst_time'];
+            if ($a['arrival_time'] === $b['arrival_time']) {
+                return $a['process_id'] <=> $b['process_id'];
+            }
+            return $a['arrival_time'] <=> $b['arrival_time'];
         });
 
         $currentTime = 0;
         $results = [];
 
-        
+        // Process the first arriving process
+        if (!empty($this->processes)) {
+            $firstProcess = array_shift($this->processes);
+            $startTime = $currentTime;
+            $endTime = $startTime + $firstProcess['burst_time'];
+            $arrivalTime = $firstProcess['arrival_time'];
+            $burstTime = $firstProcess['burst_time'];
+            $waitingTime = $startTime - $arrivalTime;
+            $turnaroundTime = $endTime - $arrivalTime;
 
+            $this->results[] = [
+                'process_id' => $firstProcess['process_id'],
+                'arrival_time' => $arrivalTime,
+                'burst_time' => $burstTime,
+                'start_time' => $startTime,
+                'end_time' => $endTime,
+                'waiting_time' => $waitingTime,
+                'turnaround_time' => $turnaroundTime
+            ];
+            $currentTime = $endTime;
+        }
+
+        // Sort remaining processes by burst time
+        usort($this->processes, function ($a, $b) {
+            return $a['burst_time'] <=> $b['burst_time'];
+        });
+
+        // Process remaining processes
         foreach ($this->processes as $process) {
             $startTime = $currentTime;
             $endTime = $startTime + $process['burst_time'];
@@ -39,7 +67,6 @@ class SJF
             ];
             $currentTime = $endTime;
         }
-
     }
     public function getResults()
     {
